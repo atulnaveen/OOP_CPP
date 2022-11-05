2937cf038c45f79b265899a0f5fba2c683773cb5
ATUL NAVEEN <114799622+atulnaveen@users.noreply.github.com>
2022-11-05 04:27:13 -0700

    Add files via upload
    Mathematical Operations on Complex Numbers

---
+++ Assign1.cpp
@@ -0,0 +1,70 @@
+/*Mathematical Operations on Complex Numbers*/
+#include<iostream>
+using namespace std;
+class complex
+{
+  float x,y;
+  public:
+   complex operator +(complex);
+   complex operator -(complex);
+   complex operator *(complex);
+   complex operator /(complex);
+   friend istream & operator >>(istream &input,complex &t)
+   {
+    cout<<"Enter Real Part:";
+    cin>>t.x;
+    cout<<"Enter Imaginary Part:";
+    cin>>t.y;
+   }
+   friend ostream & operator <<(ostream &output,complex &t)
+   {
+    output<<t.x<<"+"<<t.y<<"i"<<endl;
+   }
+};
+complex complex::operator +(complex c)
+{
+    complex temp;
+    temp.x=x+c.x;
+    temp.y=y+c.y;
+    return(temp);
+}
+complex complex::operator -(complex c)
+{
+    complex temp;
+    temp.x=x-c.x;
+    temp.y=y-c.y;
+    return(temp);
+}
+complex complex::operator *(complex c)
+{
+    complex temp;
+    temp.x=(x*c.x)-(y*c.y);
+    temp.y=(y*c.x)+(x*c.y);
+    return(temp);
+}
+complex complex::operator /(complex c)
+{
+    complex temp;
+    temp.x=((x*c.x+y*c.y)/((c.x*c.x)+(c.y*c.y)));
+    temp.y=((y*c.x-x*c.y)/((c.x*c.x)+(c.y*c.y)));
+    return(temp);
+}
+int main()
+{   complex c1,c2,c3,c4,c5,c6;
+    
+    cout<<"Enter first complex number"<<endl;
+    cin>>c1;
+    cout<<"Enter the second complex number"<<endl;
+    cin>>c2;
+    cout<<"-----------------------------------"<<endl;
+    cout<<"Complex numbers are:\n"<<c1<<c2<<endl;
+    c3=c1+c2;
+    c4=c1-c2;
+    c5=c1*c2;
+    c6=c1/c2;
+    cout<<"Addition: "<<c3;
+    cout<<"Subtraction: "<<c4;
+    cout<<"Multiplication: "<<c5;
+    cout<<"Division: "<<c6;
+    return 0;
+}
